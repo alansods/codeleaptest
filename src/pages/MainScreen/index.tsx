60724ac73b9d526e2c85a187eef15d894170b74a
Alan Santos <alansods@gmail.com>
2023-04-09 15:25:39 -0300

    try fix

--- src/pages/MainScreen/index.tsx
+++ src/pages/MainScreen/index.tsx
@@ -1,3 +1,5 @@
+export {}; // Add this line to turn the file into a module
+
 import Header from "../../components/Header";
 import CreatePost from "./components/CreatePost";
 import PostTemplate from "./components/PostTemplate";
